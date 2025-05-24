3fea2a1a60a94a6c1d8a98bd7ff7f3eed556a063
chunkityip <alex0917lfo@hotmail.com>
2025-05-23 21:29:49 -0400

    Add Entitlement

---
+++ src/main/java/employee_management/domain/enums/Entitlement.java
@@ -0,0 +1,20 @@
+package employee_management.domain.enums;
+
+
+public enum Entitlement {
+    // For Admin
+    APPROVE_HIRING,
+    REJECT_HIRING,
+
+    // HR
+    EDIT_SALARY,
+    VIEW_PAYROLL,
+    HIRE_EMPLOYEES,
+
+    // For Manager
+    ASSIGN_TASKS,
+
+    STORY_POINT_TASKS,
+
+}
+
